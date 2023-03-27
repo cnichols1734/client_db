47cea9356236552f1d36bc63a7413de100c9e37b
Christopher Nichols <christophernichols@Christophers-MacBook-Air.local>
2023-03-26 21:35:42 -0500

    Added in property db to asscoiate properties and clients and UI elements for this

--- client_db.py
+++ client_db.py
@@ -18,8 +18,37 @@ class Client(db.Model):
     client_inspection_date = db.Column(db.Date, nullable=True)
     client_closing_date = db.Column(db.Date, nullable=True)
 
+    property_detail = db.relationship('Property', backref='client', uselist=False)
+
+    @property
+    def property_name(self):
+        if self.property_detail:
+            return self.property_detail.street_name
+        else:
+            return "No Property Added"
+
     def __repr__(self):
         return f"<Client {self.client_name}>"
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    street_address = db.Column(db.String(100), nullable=False)
+    street_name = db.Column(db.String(100))
+    zip_code = db.Column(db.String(10))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    property_type = db.Column(db.String(20))
+    price = db.Column(db.Float)
+    build_year = db.Column(db.Integer)
+
+    client_property = db.relationship('Client', backref=db.backref('properties', lazy=True))
+
+    @property
+    def name(self):
+        return f"{self.street_address} {self.street_name}"
+
+
 
 @app.route('/')
 def index():
@@ -52,15 +81,37 @@ def update_client(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+
 @app.route('/client/<int:id>')
 def client(id):
     client = Client.query.get_or_404(id)
     return render_template('client.html', client=client)
 
+@app.route('/add_property/<int:client_id>', methods=['POST'])
+def add_property(client_id):
+    client = Client.query.get_or_404(client_id)
+    property = Property(
+        client_id=client.id,
+        street_address=request.form['street_address'],
+        street_name=request.form['street_name'],
+        zip_code=request.form['zip_code'],
+        city=request.form['city'],
+        state=request.form['state'],
+        property_type=request.form['property_type'],
+        price=float(request.form['price']),
+        build_year=request.form['build_year']
+    )
+    db.session.add(property)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+
 
 
 
